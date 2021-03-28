7ecb42723cf69078ac601002d936c2bb9b5c28bd
André Vennberg <andre.vennberg@gmail.com>
2021-03-28 21:08:48 +0200

    Did some improvements to the logging.

--- src/utility/logger.rs
+++ src/utility/logger.rs
@@ -1,3 +1,12 @@
+use std::{io::stdout, sync::Mutex};
+
+use crossterm::{
+    cursor, execute,
+    terminal::{self, ClearType},
+};
+use lazy_static::lazy_static;
+use log::LevelFilter;
+
 pub struct Logger {}
 
 impl Logger {
@@ -6,30 +15,52 @@ impl Logger {
 
         fern::Dispatch::new()
             .format(move |out, message, record| {
+                lazy_static! {
+                    static ref LAST_TARGET: Mutex<String> = Mutex::new(String::new());
+                }
+
+                match record.target() {
+                    "holo_bot::lib::holo_api"
+                        if record.level() == LevelFilter::Debug
+                            && *LAST_TARGET.lock().unwrap() == "holo_bot::lib::holo_api" =>
+                    {
+                        execute!(
+                            stdout(),
+                            cursor::MoveUp(1),
+                            cursor::MoveToColumn(0),
+                            terminal::Clear(ClearType::CurrentLine)
+                        )
+                        .unwrap();
+                    }
+                    _ => (),
+                };
+
                 out.finish(format_args!(
                     "{}[{}][{}] {}",
                     chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                     record.target(),
                     colours.color(record.level()),
                     message,
-                ))
+                ));
+
+                *LAST_TARGET.lock().unwrap() = record.target().to_string();
             })
             .chain(
                 fern::Dispatch::new()
-                    .level(log::LevelFilter::Info)
-                    .level_for("holo_bot", log::LevelFilter::Debug)
-                    .level_for("serenity", log::LevelFilter::Warn)
-                    .level_for("tracing", log::LevelFilter::Warn)
+                    .level(LevelFilter::Info)
+                    .level_for("holo_bot", LevelFilter::Debug)
+                    .level_for("serenity", LevelFilter::Warn)
+                    .level_for("tracing", LevelFilter::Warn)
                     .chain(std::io::stdout()),
             )
             .chain(
                 fern::Dispatch::new()
-                    .level(log::LevelFilter::Error)
+                    .level(LevelFilter::Error)
                     .chain(std::io::stderr()),
             )
             .chain(
                 fern::Dispatch::new()
-                    .level(log::LevelFilter::Trace)
+                    .level(LevelFilter::Trace)
                     .chain(fern::log_file("holo-bot.log")?)
                     .chain(
                         std::fs::OpenOptions::new()
