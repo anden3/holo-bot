3deb6de33a0574f09317305519c76fbad4b7911a
André Vennberg <andre.vennberg@uncloudy.se>
2021-06-10 13:10:58 +0200

    Improved pekofy arabic support and split the command into functions.

--- crates/commands/src/pekofy.rs
+++ crates/commands/src/pekofy.rs
@@ -21,21 +21,70 @@ use utility::regex;
 /// Pekofies replied-to message or the provided text.
 pub async fn pekofy(ctx: &Ctx, msg: &Message) -> CommandResult {
     let sentence_rgx: &'static Regex = regex!(
-        r#"(?msx)                                                           # Flags
-        (?P<text>.*?[\w&&[^_]]+.*?)                                         # Text, not including underscores at the end.
+        r#"(?msx)                                                               # Flags
+        (?P<text>.*?[\w&&[^_]]+.*?)                                             # Text, not including underscores at the end.
         (?P<punct>
-            [\.!\?\u3002\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]+       # Match punctuation not at the end of a line.
+            [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]+     # Match punctuation not at the end of a line.
             |
-            \s*(?:                                                          # Include eventual whitespace after peko.
-                [\.!\?\u3002\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]    # Match punctuation at the end of a line.
+        \s*(?:                                                                  # Include eventual whitespace after peko.
+                [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]  # Match punctuation at the end of a line.
                 |
-                (?:<:\w+:\d+>)                                              # Match Discord emotes at the end of a line.
+                (?:<a?:\w+:\d+>)                                                # Match Discord emotes at the end of a line.
                 |
-                [\x{1F600}-\x{1F64F}]                                       # Match Unicode emoji at the end of a line.
+                [\x{1F600}-\x{1F64F}]                                           # Match Unicode emoji at the end of a line.
             )*$
         )"#
     );
 
+    let text = match get_text(ctx, msg).await? {
+        Some(t) => t,
+        None => return Ok(()),
+    };
+
+    if text.starts_with("-pekofy") {
+        msg.channel_id
+            .say(&ctx.http, "Nice try peko")
+            .await
+            .context(here!())?;
+        return Ok(());
+    }
+
+    let mut pekofied_text = String::with_capacity(text.len());
+
+    for capture in sentence_rgx.captures_iter(&text) {
+        // Check if the capture is empty.
+        if capture
+            .get(0)
+            .map(|c| c.as_str().trim().is_empty())
+            .unwrap_or(true)
+        {
+            continue;
+        }
+
+        let text = capture
+            .name("text")
+            .ok_or_else(|| anyhow!("Couldn't find 'text' capture!"))
+            .context(here!())?
+            .as_str();
+
+        let response = get_peko_response(text)?;
+
+        capture.expand(&format!("$text{}$punct", response), &mut pekofied_text);
+    }
+
+    if pekofied_text.trim().is_empty() {
+        return Ok(());
+    }
+
+    msg.channel_id
+        .say(&ctx.http, pekofied_text)
+        .await
+        .context(here!())?;
+
+    Ok(())
+}
+
+async fn get_text(ctx: &Ctx, msg: &Message) -> anyhow::Result<Option<String>> {
     let mut args = Args::new(
         &msg.content_safe(&ctx.cache).await,
         &[Delimiter::Single(' ')],
@@ -50,40 +99,24 @@ pub async fn pekofy(ctx: &Ctx, msg: &Message) -> CommandResult {
         msg.delete(&ctx.http).await.context(here!())?;
     } else if let Some(src) = &msg.referenced_message {
         if src.author.bot {
-            return Ok(());
+            return Ok(None);
         }
 
         text = src.content_safe(&ctx.cache).await;
         msg.delete(&ctx.http).await.context(here!())?;
     } else {
-        return Ok(());
+        return Ok(None);
     }
 
-    if text.starts_with("-pekofy") {
-        msg.channel_id
-            .say(&ctx.http, "Nice try peko")
-            .await
-            .context(here!())?;
-        return Ok(());
-    }
+    Ok(Some(text))
+}
 
-    let mut pekofied_text = String::with_capacity(text.len());
+fn get_peko_response(text: &str) -> anyhow::Result<&str> {
+    let text_is_uppercase = text == text.to_uppercase();
 
-    for capture in sentence_rgx.captures_iter(&text) {
-        if capture.get(0).unwrap().as_str().trim().is_empty() {
-            continue;
-        }
-
-        let text = capture
-            .name("text")
-            .ok_or_else(|| anyhow!("Couldn't find 'text' capture!"))
-            .context(here!())?
-            .as_str();
-
-        let text_is_uppercase = text == text.to_uppercase();
-
-        // Get response based on alphabet used.
-        let response = match text
+    // Get response based on alphabet used.
+    Ok(
+        match text
             .chars()
             .last()
             .ok_or_else(|| anyhow!("Can't get last character!"))
@@ -125,19 +158,6 @@ pub async fn pekofy(ctx: &Ctx, msg: &Message) -> CommandResult {
                     " peko"
                 }
             }
-        };
-
-        capture.expand(&format!("$text{}$punct", response), &mut pekofied_text);
-    }
-
-    if pekofied_text.trim().is_empty() {
-        return Ok(());
-    }
-
-    msg.channel_id
-        .say(&ctx.http, pekofied_text)
-        .await
-        .context(here!())?;
-
-    Ok(())
+        },
+    )
 }
