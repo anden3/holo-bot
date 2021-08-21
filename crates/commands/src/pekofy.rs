81602ca75cd99b6a305094c46e569d6dc8fc1a91
André Vennberg <andre.vennberg@uncloudy.se>
2021-08-21 18:14:43 +0200

    Added embed support to pekofy.

--- crates/commands/src/pekofy.rs
+++ crates/commands/src/pekofy.rs
@@ -1,8 +1,28 @@
 use regex::Regex;
+use serenity::{
+    builder::{CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateMessage},
+    model::channel::{Embed, EmbedAuthor, EmbedField, EmbedFooter},
+};
 
 use super::prelude::*;
 
-use utility::regex;
+use utility::regex_lazy;
+
+static SENTENCE_RGX: once_cell::sync::Lazy<Regex> = regex_lazy!(
+    r#"(?msx)                                                               # Flags
+        (?P<text>.*?[\w&&[^_]]+.*?)                                             # Text, not including underscores at the end.
+        (?P<punct>
+            [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]+     # Match punctuation not at the end of a line.
+            |
+        \s*(?:                                                                  # Include eventual whitespace after peko.
+                [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]  # Match punctuation at the end of a line.
+                |
+                (?:<a?:\w+:\d+>)                                                # Match Discord emotes at the end of a line.
+                |
+                [\x{1F600}-\x{1F64F}]                                           # Match Unicode emoji at the end of a line.
+            )*$
+        )"#
+);
 
 #[command]
 #[allowed_roles(
@@ -20,38 +40,50 @@ use utility::regex;
 )]
 /// Pekofies replied-to message or the provided text.
 pub async fn pekofy(ctx: &Ctx, msg: &Message) -> CommandResult {
-    let sentence_rgx: &'static Regex = regex!(
-        r#"(?msx)                                                               # Flags
-        (?P<text>.*?[\w&&[^_]]+.*?)                                             # Text, not including underscores at the end.
-        (?P<punct>
-            [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]+     # Match punctuation not at the end of a line.
-            |
-        \s*(?:                                                                  # Include eventual whitespace after peko.
-                [\.!\?\u3002\u0629\uFE12\uFE52\uFF0E\uFF61\uFF01\uFF1F"_\*`\)]  # Match punctuation at the end of a line.
-                |
-                (?:<a?:\w+:\d+>)                                                # Match Discord emotes at the end of a line.
-                |
-                [\x{1F600}-\x{1F64F}]                                           # Match Unicode emoji at the end of a line.
-            )*$
-        )"#
-    );
+    let mut reply = CreateMessage::default();
 
-    let text = match get_text(ctx, msg).await? {
-        Some(t) => t,
+    let (text, embeds) = match get_data(ctx, msg).await? {
+        Some((text, embeds)) => (text, embeds),
         None => return Ok(()),
     };
 
-    if text.starts_with("-pekofy") {
+    let mut result = Ok(());
+
+    if let Some(text) = text {
+        result = result.and(if text.starts_with("-pekofy") {
+            Err(anyhow!("Nice try peko"))
+        } else {
+            pekofy_text(&text).map(|text| {
+                reply.content(text);
+            })
+        });
+    }
+
+    if !embeds.is_empty() {
+        result = result.and(pekofy_embeds(msg, &mut reply).await);
+    }
+
+    if let Err(e) = result {
         msg.channel_id
-            .say(&ctx.http, "Nice try peko")
+            .say(&ctx.http, e.to_string())
             .await
             .context(here!())?;
+
         return Ok(());
     }
 
+    msg.channel_id
+        .send_message(&ctx.http, |_| &mut reply)
+        .await
+        .context(here!())?;
+
+    Ok(())
+}
+
+fn pekofy_text(text: &str) -> anyhow::Result<String> {
     let mut pekofied_text = String::with_capacity(text.len());
 
-    for capture in sentence_rgx.captures_iter(&text) {
+    for capture in SENTENCE_RGX.captures_iter(&text) {
         // Check if the capture is empty.
         if capture
             .get(0)
@@ -72,19 +104,96 @@ pub async fn pekofy(ctx: &Ctx, msg: &Message) -> CommandResult {
         capture.expand(&format!("$text{}$punct", response), &mut pekofied_text);
     }
 
-    if pekofied_text.trim().is_empty() {
-        return Ok(());
-    }
+    Ok(pekofied_text)
+}
 
-    msg.channel_id
-        .say(&ctx.http, pekofied_text)
-        .await
-        .context(here!())?;
+async fn pekofy_embeds(msg: &Message, reply: &mut CreateMessage<'_>) -> anyhow::Result<()> {
+    reply.set_embeds(
+        msg.embeds
+            .iter()
+            .map(|e| pekofy_embed(&e))
+            .collect::<anyhow::Result<_>>()?,
+    );
 
     Ok(())
 }
 
-async fn get_text(ctx: &Ctx, msg: &Message) -> anyhow::Result<Option<String>> {
+fn pekofy_embed(embed: &Embed) -> anyhow::Result<CreateEmbed> {
+    let mut peko_embed = CreateEmbed::default();
+
+    if let Some(EmbedAuthor {
+        name,
+        icon_url,
+        url,
+        ..
+    }) = &embed.author
+    {
+        let mut peko_author = CreateEmbedAuthor::default();
+
+        peko_author.name(pekofy_text(&name)?);
+
+        if let Some(icon_url) = icon_url {
+            peko_author.icon_url(icon_url);
+        }
+
+        if let Some(url) = url {
+            peko_author.url(url);
+        }
+
+        peko_embed.set_author(peko_author);
+    }
+
+    if let Some(EmbedFooter { text, icon_url, .. }) = &embed.footer {
+        let mut peko_footer = CreateEmbedFooter::default();
+
+        peko_footer.text(pekofy_text(&text)?);
+
+        if let Some(icon_url) = icon_url {
+            peko_footer.icon_url(icon_url);
+        }
+
+        peko_embed.set_footer(peko_footer);
+    }
+
+    if let Some(title) = &embed.title {
+        peko_embed.title(pekofy_text(&title)?);
+    }
+
+    if let Some(description) = &embed.description {
+        peko_embed.description(pekofy_text(&description)?);
+    }
+
+    if !embed.fields.is_empty() {
+        peko_embed.fields(
+            embed
+                .fields
+                .iter()
+                .map(
+                    |EmbedField {
+                         name,
+                         value,
+                         inline,
+                         ..
+                     }| {
+                        match [pekofy_text(&name), pekofy_text(&value)] {
+                            [Ok(name), Ok(value)] => Ok((name, value, *inline)),
+                            [Err(n), Err(v)] => Err(n).context(v),
+                            [Err(e), _] | [_, Err(e)] => Err(e),
+                        }
+                    },
+                )
+                .collect::<anyhow::Result<Vec<_>>>()?,
+        );
+    }
+
+    Ok(peko_embed)
+}
+
+#[allow(clippy::needless_lifetimes)]
+async fn get_data<'a>(
+    ctx: &Ctx,
+    msg: &'a Message,
+) -> anyhow::Result<Option<(Option<String>, &'a Vec<Embed>)>> {
     let mut args = Args::new(
         &msg.content_safe(&ctx.cache).await,
         &[Delimiter::Single(' ')],
@@ -93,22 +202,41 @@ async fn get_text(ctx: &Ctx, msg: &Message) -> anyhow::Result<Option<String>> {
     args.advance();
 
     let text;
+    let embeds;
 
-    if let Some(remains) = args.remains() {
-        text = remains.to_owned();
-        msg.delete(&ctx.http).await.context(here!())?;
-    } else if let Some(src) = &msg.referenced_message {
+    if let Some(src) = &msg.referenced_message {
         if src.author.bot {
             return Ok(None);
         }
 
-        text = src.content_safe(&ctx.cache).await;
+        embeds = &src.embeds;
+
+        let safe_text = src.content_safe(&ctx.cache).await;
+
+        text = if safe_text.trim().is_empty() {
+            if embeds.is_empty() {
+                return Ok(None);
+            }
+
+            None
+        } else {
+            Some(safe_text)
+        };
+
         msg.delete(&ctx.http).await.context(here!())?;
     } else {
-        return Ok(None);
+        embeds = &msg.embeds;
+
+        text = match args.remains() {
+            Some(remains) => Some(remains.to_owned()),
+            None if embeds.is_empty() => return Ok(None),
+            None => None,
+        };
+
+        msg.delete(&ctx.http).await.context(here!())?;
     }
 
-    Ok(Some(text))
+    Ok(Some((text, embeds)))
 }
 
 fn get_peko_response(text: &str) -> anyhow::Result<&str> {
--- crates/utility/src/macros.rs
+++ crates/utility/src/macros.rs
@@ -8,9 +8,16 @@ macro_rules! here {
 #[macro_export]
 macro_rules! regex {
     ($re:literal $(,)?) => {{
-        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
-        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+        static RE: ::once_cell::sync::OnceCell<::regex::Regex> = ::once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| ::regex::Regex::new($re).unwrap())
     }};
+}
+
+#[macro_export]
+macro_rules! regex_lazy {
+    ($re:literal $(,)?) => {
+        ::once_cell::sync::Lazy::<::regex::Regex>::new(|| regex::Regex::new($re).unwrap());
+    };
 }
 
 #[macro_export]
