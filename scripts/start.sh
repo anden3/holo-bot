d94a039b228a5611e71f42e073f028c3ab5aa279
André Vennberg <andre.vennberg@uncloudy.se>
2021-10-05 14:13:09 +0200

    Removed unused scripts.

--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-readonly TARGET_HOST=pi@rpi
-readonly TARGET_PATH=/home/pi/Documents/Rust/holo-bot
-readonly TARGET_EXEC=holo-bot
-readonly SERVICE_NAME=$TARGET_EXEC.service
-
-ssh -t ${TARGET_HOST} "systemctl is-active --quiet ${SERVICE_NAME} && sudo systemctl stop ${SERVICE_NAME}; cd ${TARGET_PATH}; RUST_BACKTRACE=1 ./${TARGET_EXEC}"
